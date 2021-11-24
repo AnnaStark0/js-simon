a46b9596d060ea4e95cd5e45d4ccfba3690625a2
Annastark <89winteriscoming@gmail.com>
2021-11-24 15:27:49 +0100

    Init JS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Carissimi ragazzi,
 // eccoci all'ultimo esercizio di JavaScript
+
 // Esercizio di oggi: Simon Says nome repo: js-simon
 // Descrizione:
 // Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. 
@@ -11,5 +12,25 @@
 // * Pensate prima in italiano.
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
 // Buon divertimento :party_blob:
 // Ancora un grande abbraccio :faccia_sorridente_con_3_cuori:
+
+
+
+// let name = prompt ('Inserisci il tuo Nome');
+
+// alert (`Ciao ${name} ! Pront* per Simon Says? ` );
+
+
+let arrayRandom = [];
+
+// - Genero 5 Numeri Random -
+
+for(let i = 0; i <= 5; i++) {
+    let numRandom = Math.floor(Math.random() * 100);
+    console.log(numRandom);
+}
+
+
+
